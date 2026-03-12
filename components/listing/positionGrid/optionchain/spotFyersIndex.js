64fb1b4f9347b2bf9834961da2bf72e03dae1f6f
Vinayak Anvekar <fairvinay@gmail.com>
2026-03-12 14:50:12 +0530

    refrence to ~artillery2feed.com edited to artilleryfeed2.onrender.com

--- components/listing/positionGrid/optionchain/spotFyersIndex.js
+++ components/listing/positionGrid/optionchain/spotFyersIndex.js
@@ -2,7 +2,7 @@
 
 //import fetch from "node-fetch";
 
-const NSE_URL = "https://artillery2feed.com/";
+const NSE_URL = "https://artilleryfeed2.onrender.com/";
 const FYERS_URL = "https://fyersfeed.onrender.com/stream";
 
 const FYERS_SYMBOL = "NSE:NIFTY50-INDEX";
